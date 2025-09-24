4bd982efa2ae003bee0327ef007a0bd90ec818fe
Jinag Jundong <jiangjundong@yandex.ru>
2025-09-24 11:19:57 +0300

    1st version

--- calculator.cpp
+++ calculator.cpp
@@ -1,21 +1,23 @@
 // В этом файле определения функций.
 // За основу возьмите решение предыдущей задачи.
-//  Функция ReadNumber, которая будет читать число и выводить сообщение об ошибке в std::cerr в случае неудачи.
-//  Функция RunCalculatorCycle, которая будет содержать всю логику калькулятора.
+//  Функция ReadNumber, которая будет читать число и выводить сообщение об
+//  ошибке в std::cerr в случае неудачи. Функция RunCalculatorCycle, которая
+//  будет содержать всю логику калькулятора.
 #include "calculator.h"
 #include <iostream>
+#include <math.h>
 
 using Number = double;
 
 // Возвращает true, если удалось прочитать число
 // и сохранить его в Number.
 
-bool ReadNumber(Number& result){
+bool ReadNumber(Number &result) {
 
     Number n;
     std::cin >> n;
 
-    if (std::cin.good()){
+    if (std::cin.good()) {
         result = n;
         return true;
     } else {
@@ -24,35 +26,57 @@ bool ReadNumber(Number& result){
     }
 }
 
-bool DoOperation(Number& cur_sum, Number& left, Number& right, Number& saved, bool& checked){
+bool DoOperation(Number &cur_sum, Number &left, Number &right, Number &saved,
+                 bool &checked) {
 
     char operation;
     std::cin >> operation;
+    bool is_pow = false;
+
+    if (operation == 'q') {
+        checked = false;
+        return false;
+    } else if (operation == 'c') {
+        saved = 0;
+    } else if (operation == 's') {
+        saved = cur_sum;
+    } else if (operation == 'l') {
+        cur_sum = saved;
+    } else if (operation == ':') {
+        std::cin >> cur_sum;
+    } else if (operation == '*') {
+        char ch;
+        std::cin >> ch;
+        if (ch == '*') {
+            is_pow = true;
+        }
+    }
 
     if (operation == '=') {
         cur_sum += left;
-        left=0;
+        left = 0;
         std::cout << cur_sum << std::endl;
-    } else if (operation == '+') {
+        return true;
+    }
+
+    checked = ReadNumber(right);
+
+    if (operation == '+') {
         cur_sum += left;
         left = right;
     } else if (operation == '-') {
         cur_sum += left;
         left = -right;
     } else if (operation == '*') {
-        left *= right;
+        if (is_pow == true) {
+            left = pow(left, right);
+        } else {
+            left *= right;
+        }
     } else if (operation == '/') {
         left /= right;
-    } else if (operation == 'c'){
-        saved=0;
-    } else if (operation =='q'){
-        checked = false;
-    } else if (operation =='s'){
-        saved = cur_sum;
-    } else if (operation =='l'){
-        cur_sum = saved;
-    } else if (operation ==':'){
-        std::cin >> cur_sum;
+    } else if (operation == '/') {
+        left /= right;
     } else {
         std::cerr << "Error: Unknown token " << operation << std::endl;
         return false;
@@ -60,24 +84,21 @@ bool DoOperation(Number& cur_sum, Number& left, Number& right, Number& saved, bo
     return true;
 }
 
-
-
 // Возвращает true, если работа завершилась штатно
 // командой q. Если из-за ошибки, возвращает false.
-bool RunCalculatorCycle (){
+bool RunCalculatorCycle() {
 
-    Number saved=0;
+    Number saved = 0;
     Number cur_sum = 0;
-    Number left=0, right=0;
+    Number left = 0, right = 0;
 
     bool checked = true;
     char cmd;
 
-    ReadNumber(left);
+    checked = ReadNumber(left);
 
     while (checked) {
-        DoOperation(cur_sum,left,right,saved, checked);
-        ReadNumber(right);
+        checked = DoOperation(cur_sum, left, right, saved, checked);
     }
 
     return true;
