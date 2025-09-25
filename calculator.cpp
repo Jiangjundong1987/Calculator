1a344510d72b4ea99bb5b82e7d595799e9ae3b64
Jinag Jundong <jiangjundong@yandex.ru>
2025-09-25 10:03:45 +0300

    final

--- calculator.cpp
+++ calculator.cpp
@@ -7,6 +7,10 @@
 
 using Number = double;
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> 262dfad (like final)
 bool ReadNumber(Number &result) {
 
     Number n;
@@ -21,8 +25,26 @@ bool ReadNumber(Number &result) {
     }
 }
 
+<<<<<<< HEAD
 bool DoOperation(Number &cur_sum, Number &left, Number &right, Number &saved,
                  bool &checked, bool &is_saved) {
+=======
+std::string ReadComand(std::string comand) {
+
+    std::string cmd;
+    for (size_t i = 0; i < comand.size(); i++) {
+        if (((comand[i] >= 48) && (comand[i] <= 57))) {
+            break;
+        } else {
+            cmd.push_back(comand[i]);
+        }
+    }
+    return cmd;
+}
+
+bool DoOperation(Number &cur_sum, Number &right, Number &saved, bool &checked,
+                 bool &is_saved) {
+>>>>>>> 262dfad (like final)
 
     char operation;
     std::cin >> operation;
@@ -46,12 +68,18 @@ bool DoOperation(Number &cur_sum, Number &left, Number &right, Number &saved,
         std::cout << cur_sum << std::endl;
         return true;
     } else if (operation == ':') {
+<<<<<<< HEAD
         std::cin >> left;
         cur_sum = 0;
         return true;
     } else if (operation == '=') {
         cur_sum += left;
         left = 0;
+=======
+        RunCalculatorCycle();
+        return true;
+    } else if (operation == '=') {
+>>>>>>> 262dfad (like final)
         std::cout << cur_sum << std::endl;
         return true;
     } else if (operation == '+') {
@@ -60,21 +88,30 @@ bool DoOperation(Number &cur_sum, Number &left, Number &right, Number &saved,
         if (checked == false) {
             return false;
         }
+<<<<<<< HEAD
         cur_sum += left;
         left = right;
+=======
+        cur_sum += right;
+>>>>>>> 262dfad (like final)
     } else if (operation == '-') {
         checked = ReadNumber(right);
 
         if (checked == false) {
             return false;
         }
+<<<<<<< HEAD
         cur_sum += left;
         left = -right;
+=======
+        cur_sum -= right;
+>>>>>>> 262dfad (like final)
     } else if (operation == '*') {
         char ch;
         std::cin >> ch;
         if (ch == '*') {
             is_pow = true;
+<<<<<<< HEAD
         } else {
             std::cin.putback(ch);
         }
@@ -87,8 +124,22 @@ bool DoOperation(Number &cur_sum, Number &left, Number &right, Number &saved,
 
         if (is_pow) {
             left = pow(left, right);
+=======
+>>>>>>> 262dfad (like final)
         } else {
-            left *= right;
+            std::cin.putback(ch);
+        }
+
+        checked = ReadNumber(right);
+
+        if (checked == false) {
+            return false;
+        }
+
+        if (is_pow) {
+            cur_sum = pow(cur_sum, right);
+        } else {
+            cur_sum *= right;
         }
     } else if (operation == '/') {
         checked = ReadNumber(right);
@@ -96,9 +147,15 @@ bool DoOperation(Number &cur_sum, Number &left, Number &right, Number &saved,
         if (checked == false) {
             return false;
         }
+<<<<<<< HEAD
         left /= right;
+=======
+        cur_sum /= right;
+>>>>>>> 262dfad (like final)
     } else {
-        std::cerr << "Error: Unknown token " << operation << std::endl;
+        std::string token;
+        std::cin >> token;
+        std::cerr << "Error: Unknown token " << operation << ReadComand(token) << std::endl;
         return false;
     }
     return true;
@@ -108,16 +165,25 @@ bool RunCalculatorCycle() {
 
     Number saved = 0;
     Number cur_sum = 0;
+<<<<<<< HEAD
     Number left = 0, right = 0;
     bool is_saved = false;
     bool checked = true;
+=======
+    Number right = 0;
+    bool is_saved = false;
+>>>>>>> 262dfad (like final)
     char cmd;
+    bool checked;
 
-    checked = ReadNumber(left);
+    checked = ReadNumber(cur_sum);
 
     while (checked) {
+<<<<<<< HEAD
         checked = DoOperation(cur_sum, left, right, saved, checked, is_saved);
+=======
+        checked = DoOperation(cur_sum, right, saved, checked, is_saved);
+>>>>>>> 262dfad (like final)
     }
-
     return true;
 }
--- calculator.h
+++ calculator.h
@@ -1,6 +1,8 @@
 // В этом файле объявления функций.
+#include <string>
 using Number = double;
 
-bool ReadNumber(Number& result);
-bool DoOperation(Number& cur_sum, Number& left, Number& right, Number& saved, bool& checked);
+bool ReadNumber(Number &result);
+std::string ReadComand(std::string comand);
+bool DoOperation(Number &cur_sum, Number &right, Number &saved, bool &checked, bool &is_saved);
 bool RunCalculatorCycle ();
